011c485ac26d94f280e417ab86e49590fbb49cf3
Nom-an <nomanvi123@gmail.com>
2026-08-30 23:07:08 +0600

    uses of fetch

--- api.js
+++ api.js
@@ -1,7 +1,11 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+document.getElementById('btn'),addEventListener('click', function(){
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then((res)=>{
  return   res.json();
 })
 .then((data)=>{
    return console.log(data);
+})
+
 })
