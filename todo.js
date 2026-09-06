c86572e54546010b707201ee4d69428c71bcd88e
Nom-an <nomanvi123@gmail.com>
2026-09-06 22:23:36 +0600

    todo list

---
+++ todo.js
@@ -0,0 +1,43 @@
+const loadTodo = ()=>{
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>getTodo(data))
+    
+}
+
+loadTodo();
+
+// completed
+// : 
+// false
+// id
+// : 
+// 94
+// title
+// : 
+// "facilis modi saepe mollitia"
+// userId
+// : 
+// 5
+// [[Prototype]]
+// : 
+// Object
+
+const getTodo = (todos)=>{
+
+    const todoContainer = document.getElementById('todo-container');
+    todoContainer.innerText= "";
+    todos.forEach(todo=>{
+        const div = document.createElement('div');
+        div.innerHTML = `
+         <div class="todo-card">
+            <h2>${todo.id}</h2>
+            <h3>${todo.title}</h3>
+            <p>${todo.completed}</p>
+         </div>
+        
+        `
+        todoContainer.append(div);
+    })
+}
